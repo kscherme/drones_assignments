1ac1efe88bf1a497d7443fd36c4157f6049654c2
Katie Schermerhorn <kscherme@nd.edu>
2018-02-05 11:31:06 -0500

    all coords

--- homework_2b.py
+++ homework_2b.py
@@ -34,6 +34,9 @@ if not connection_string:
 print('Connecting to vehicle on: %s' % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
 
+def location_callback(self, attr_name, msg):
+    print "Location (Global): ", msg
+
 def arm_and_takeoff(aTargetAltitude):
 
 	print("Basic pre-arm checks")
