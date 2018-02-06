813df9db9ba5a57b23da4c3d9fb2bb7ee543bc21
Katie Schermerhorn <kscherme@nd.edu>
2018-02-05 19:17:43 -0500

    all coords

--- homework_2b.py
+++ homework_2b.py
@@ -87,14 +87,8 @@ for i in xrange(20,0,-1):
 	time.sleep(1)
 	loc_obj = vehicle.location.global_frame
 	print(loc_obj.lat)
-	f.write("\n\n %f," % loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
 	f.write("%f" % loc_obj.lon)
-	# loc_string.split("=")
-	# coord1 = loc_string[1].split[","][0]
-	# coord2 = loc_string[2].split[","][0]
-	# print(coord1+","+coord2)
-
-f.close()
 
 
 print("Going towards point for 10 seconds ...")
@@ -102,322 +96,552 @@ point1 = LocationGlobalRelative(41.71480,-86.24307, 15)
 vehicle.simple_goto(point1)
 
 # sleep so we can see the change in map
-time.sleep(10)
+for i in xrange(10,0,-1):
+	time.sleep(10)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point2 = LocationGlobalRelative(41.71484,-86.24306, 15)
 vehicle.simple_goto(point2, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards third point for 5 seconds (groundspeed set to 10 m/s) ...")
 point3 = LocationGlobalRelative(41.71485,-86.24305, 15)
 vehicle.simple_goto(point3, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point4 = LocationGlobalRelative(41.71486,-86.24304, 15)
 vehicle.simple_goto(point4, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point5 = LocationGlobalRelative(41.71487,-86.24300, 15)
 vehicle.simple_goto(point5, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point6 = LocationGlobalRelative(41.71486,-86.24297, 15)
 vehicle.simple_goto(point6, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point7 = LocationGlobalRelative(41.71485,-86.24295, 15)
 vehicle.simple_goto(point7, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point8 = LocationGlobalRelative(41.71484,-86.24294, 15)
 vehicle.simple_goto(point8, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71480,-86.24293, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71480,-86.24257, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(10)
+for i in xrange(10,0,-1):
+	time.sleep(10)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71476,-86.24256, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71475,-86.24255, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71474,-86.24254, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71473,-86.24250, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71474,-86.24247, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71475,-86.24245, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71476,-86.24244, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71480,-86.24243, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71480,-86.24207, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(10)
+for i in xrange(10,0,-1):
+	time.sleep(10)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71484,-86.24206, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71485,-86.24205, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71486,-86.24204, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71487,-86.24200, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71486,-86.24197, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71485,-86.24195, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71484,-86.24194, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71480,-86.24193, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71480,-86.24157, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(10)
+for i in xrange(10,0,-1):
+	time.sleep(10)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71476,-86.24156, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71475,-86.24155, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71474,-86.24154, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71473,-86.24150, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71474,-86.24147, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71475,-86.24145, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71476,-86.24144, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71480,-86.24143, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71480,-86.24107, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(10)
+for i in xrange(10,0,-1):
+	time.sleep(10)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71484,-86.24106, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71485,-86.24105, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71486,-86.24104, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71487,-86.24100, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71486,-86.24097, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71485,-86.24095, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71484,-86.24094, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards second point for 5 seconds (groundspeed set to 10 m/s) ...")
 point9 = LocationGlobalRelative(41.71480,-86.24093, 15)
 vehicle.simple_goto(point9, groundspeed=10)
 
 # sleep so we can see the change in map
-time.sleep(3)
+for i in xrange(3,0,-1):
+	time.sleep(3)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 print("Going towards first point for 5 seconds ...")
 point1 = LocationGlobalRelative(41.71435,-86.24093, 15)
 vehicle.simple_goto(point1)
 
 # sleep so we can see the change in map
-time.sleep(10)
+for i in xrange(10,0,-1):
+	time.sleep(10)
+	loc_obj = vehicle.location.global_frame
+	print(loc_obj.lat)
+	f.write("\n%f," % loc_obj.lat)
+	f.write("%f" % loc_obj.lon)
 
 
 
