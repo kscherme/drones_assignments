cf1eabbe5ec4ccace36976238fe5cbafe6ec5a3f
lukejbrennan <lukejohnbrennan@gmail.com>
2018-02-25 13:04:13 -0500

    arm_and_takeoff

--- homework4/main.py
+++ homework4/main.py
@@ -138,6 +138,40 @@ class RepeatedTimer(object):
 
 
 
+def arm_and_takeoff(vehicle, aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto
+    #  (otherwise the command after Vehicle.simple_takeoff will execute
+    #   immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
 
 
 def main(path_to_config, ardupath=None):
@@ -232,9 +266,8 @@ def main(path_to_config, ardupath=None):
 				vehicle.armed = True
 
 		#Takeoff
-		vehicle.simple_takeoff(20)
-		print("Starting drone: ", vehicle)
-		done.append(False)
+		arm_simple_takeoff(vehicle, 20)
+		time.sleep(10)
 		curr_dest.append(-1)
 
 		location = vehicle.location.global_relative_frame
@@ -268,7 +301,7 @@ def main(path_to_config, ardupath=None):
 				curr_dest[i] += 1               
 				vehicle.simple_goto(dronekit.LocationGlobalRelative(routes[i][way_number][0], routes[i][way_number][1], routes[i][way_number][2]))
 
-
+		time.sleep(2.0)
 
 
 
